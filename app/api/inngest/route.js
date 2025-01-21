5c8b4e5f979aeaafb3d11df78dd7e29f00a4c0ab
peram divya <peramdivya@perams-MacBook-Pro.local>
2025-01-21 14:30:45 +0100

    Preparing for deployment

---
+++ app/api/inngest/route.js
@@ -0,0 +1,20 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+import { checkBudgetAlert,
+  triggerRecurringTransactions,
+  processRecurringTransaction,
+  generateMonthlyReports,} from "@/lib/inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    checkBudgetAlert,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
+    generateMonthlyReports,
+  ],
+}); 
+
+
+   
+   
